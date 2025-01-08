756034a6b585e0140443a6ea96cef71ddc52e180
natalia vidal <nataliapatriciavidal@gmail.com>
2025-01-08 11:30:12 -0300

    resumen de media

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+function openImageModal(imgElement) {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imgElement.src; // Toma la imagen desde el carrusel
+    modal.style.display = 'flex'; // Muestra el modal
+}
+
+function closeImageModal() {
+    const modal = document.getElementById('imageModal');
+    modal.style.display = 'none'; // Oculta el modal
+}
